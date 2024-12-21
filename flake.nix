7a4445f93825291d23a8e72a45c47a3410563236
éclairevoyant <848000+eclairevoyant@users.noreply.github.com>
2024-12-21 09:41:48 -0500

    flake: set formatter

--- flake.nix
+++ flake.nix
@@ -1,12 +1,18 @@
 {
   inputs.nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
 
-  outputs = {self, ...} @ inputs: {
+  outputs = {
+    self,
+    nixpkgs,
+    ...
+  } @ inputs: {
     nixosModules = {
       hjem = ./modules/nixos.nix;
       default = self.nixosModules.hjem;
     };
 
     checks = import ./checks {inherit inputs;};
+
+    formatter = nixpkgs.lib.genAttrs ["x86_64-linux" "aarch64-linux"] (system: nixpkgs.legacyPackages.${system}.alejandra);
   };
 }
